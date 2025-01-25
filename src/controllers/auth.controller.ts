6153d10064314b6b1c3da3b5acf352f3221e2744
Samarpit <nsamarpit25@gmail.com>
2025-01-25 05:42:32 +0530

    feat: enhance logout functionality with secure cookie options and add Stripe integration

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -106,7 +106,14 @@ export const sendProfileInfo: RequestHandler = (req, res) => {
 };
 
 export const logout: RequestHandler = (req, res) => {
- res.clearCookie("authToken").send();
+ res
+  .clearCookie("authToken", {
+   httpOnly: true,
+   secure: process.env.NODE_ENV !== "development",
+   sameSite: process.env.NODE_ENV === "development" ? "strict" : "none",
+   path: "/",
+  })
+  .send();
 };
 
 export const updateProfile: RequestHandler = async (req, res) => {
