c1148a2bac00d4fa3b4b453b069f72d8fe9b0f6a
Samarpit <nsamarpit25@gmail.com>
2025-01-26 14:17:58 +0530

    fix: refactor author controller for improved readability and consistency

--- src/controllers/author.controller.ts
+++ src/controllers/author.controller.ts
@@ -7,88 +7,88 @@ import { RequestHandler } from "express";
 import { BookDoc } from "@/models/book.model";
 
 export const registerAuthor: RequestAuthorHandler = async (req, res) => {
-  const { body, user } = req;
-  if (!user.signedUp) {
-    return sendErrorResponse({
-      message: "User must be signed up before registering as an author!!",
-      res,
-      status: 401,
-    });
-  }
-
-  const newAuthor = new AuthorModel({
-    name: body.name,
-    about: body.about,
-    userId: user.id,
-    socialLinks: body.socialLinks,
+ const { body, user } = req;
+ if (!user.signedUp) {
+  return sendErrorResponse({
+   message: "User must be signed up before registering as an author!!",
+   res,
+   status: 401,
   });
+ }
 
-  const uniqueSlug = slugify(`${newAuthor.name} ${newAuthor._id}`, {
-    lower: true,
-    replacement: "-",
-  });
+ const newAuthor = new AuthorModel({
+  name: body.name,
+  about: body.about,
+  userId: user.id,
+  socialLinks: body.socialLinks,
+ });
 
-  newAuthor.slug = uniqueSlug;
-  await newAuthor.save();
+ const uniqueSlug = slugify(`${newAuthor.name} ${newAuthor._id}`, {
+  lower: true,
+  replacement: "-",
+ });
 
-  await userModel.findByIdAndUpdate(user.id, {
-    authorId: newAuthor._id,
-    role: "author",
-  });
+ newAuthor.slug = uniqueSlug;
+ await newAuthor.save();
 
-  res.json({ message: "Thanks for registering as an author." });
+ await userModel.findByIdAndUpdate(user.id, {
+  authorId: newAuthor._id,
+  role: "author",
+ });
+
+ res.json({ message: "Thanks for registering as an author." });
 };
 
 export const getAuthorDetails: RequestHandler = async (req, res) => {
-  const { id } = req.params;
+ const { id } = req.params;
 
-  const author = await AuthorModel.findById(id)
-    .populate<{ userId: UserDoc }>("userId")
-    .populate<{ books: BookDoc[] }>("books");
-  // console.log(author);
-  if (!author)
-    return sendErrorResponse({
-      res,
-      message: "Author not found!",
-      status: 404,
-    });
-
-  let avatar = "";
-  if (author.userId?.avatar?.url) {
-    avatar = author.userId.avatar?.url;
-  }
-
-  res.json({
-    id: author._id,
-    name: author.name,
-    about: author.about,
-    socialLinks: author.socialLinks,
-    avatar,
-    books: author.books?.map((book) => {
-      return {
-        id: book._id?.toString(),
-        title: book.title,
-        slug: book.slug,
-        genre: book.genre,
-        price: {
-          mrp: (book.price.mrp / 100).toFixed(2),
-          sale: (book.price.sale / 100).toFixed(2),
-        },
-        cover: book.cover?.url,
-        rating: book.averageRating?.toFixed(1),
-      };
-    }),
+ const author = await AuthorModel.findById(id)
+  .populate<{ userId: UserDoc }>("userId")
+  .populate<{ books: BookDoc[] }>("books");
+ // console.log(author);
+ if (!author)
+  return sendErrorResponse({
+   res,
+   message: "Author not found!",
+   status: 404,
   });
+
+ let avatar = "";
+ if (author.userId?.avatar?.url) {
+  avatar = author.userId.avatar?.url;
+ }
+
+ res.json({
+  id: author._id,
+  name: author.name,
+  about: author.about,
+  socialLinks: author.socialLinks,
+  avatar,
+  books: author.books?.map((book) => {
+   return {
+    id: book._id?.toString(),
+    title: book.title,
+    slug: book.slug,
+    genre: book.genre,
+    price: {
+     mrp: (book.price.mrp / 100).toFixed(2),
+     sale: (book.price.sale / 100).toFixed(2),
+    },
+    cover: book.cover?.url,
+    rating: book.averageRating?.toFixed(1),
+   };
+  }),
+ });
 };
 
 export const updateAuthor: RequestAuthorHandler = async (req, res) => {
-  const { body, user } = req;
+ const { body, user } = req;
 
-  await AuthorModel.findByIdAndUpdate(user.authorId, {
-    name: body.name,
-    about: body.about,
-    socialLinks: body.socialLinks,
-  });
+ await AuthorModel.findByIdAndUpdate(user.authorId, {
+  name: body.name,
+  about: body.about,
+  socialLinks: body.socialLinks,
+ });
 
-  res.json({ message: "Your details have been updated successfully." });
+ res.json({ message: "Your details have been updated successfully." });
 };
