4c6b27356a9696f7ff291af24a70a0414d4ba547
Samarpit <nsamarpit25@gmail.com>
2025-01-27 17:30:10 +0530

    fix: refactor checkout controller for improved readability and error handling

--- src/controllers/checkout.controller.ts
+++ src/controllers/checkout.controller.ts
@@ -9,182 +9,183 @@ import Stripe from "stripe";
 
 type StripeLineItems = Stripe.Checkout.SessionCreateParams.LineItem[];
 type options = {
- customer: Stripe.CustomerCreateParams;
- lineItems: StripeLineItems;
+  customer: Stripe.CustomerCreateParams;
+  lineItems: StripeLineItems;
 };
 
 const generateStripeCheckoutSession = async (options: options) => {
- const customer = await stripe.customers.create(options.customer);
+  const customer = await stripe.customers.create(options.customer);
 
- const session = await stripe.checkout.sessions.create({
-  mode: "payment",
-  payment_method_types: ["card"],
-  success_url: process.env.PAYMENT_SUCCESS_URL,
-  cancel_url: process.env.PAYMENT_CANCEL_URL,
-  line_items: options.lineItems,
-  customer: customer.id,
- });
+  const session = await stripe.checkout.sessions.create({
+    mode: "payment",
+    payment_method_types: ["card"],
+    success_url: process.env.PAYMENT_SUCCESS_URL,
+    cancel_url: process.env.PAYMENT_CANCEL_URL,
+    line_items: options.lineItems,
+    customer: customer.id,
+  });
 
- return session;
+  return session;
 };
 
 export const checkout: RequestHandler = async (req, res) => {
- const { cartId } = req.body;
+  const { cartId } = req.body;
 
- if (!isValidObjectId(cartId)) {
-  return sendErrorResponse({
-   message: "Invalid cart id!!",
-   res,
-   status: 401,
-  });
- }
-
- const cart = await CartModel.findOne({
-  _id: cartId,
-  userId: req.user.id,
- }).populate<{
-  items: { product: BookDoc; quantity: number }[];
- }>({ path: "items.product" });
-
- if (!cart) {
-  return sendErrorResponse({
-   message: "cart not found",
-   res,
-   status: 404,
-  });
- }
-
- const newOrder = await OrderModel.create({
-  userId: req.user.id,
-  orderItems: cart.items.map(({ product, quantity }) => {
-   return {
-    id: product._id,
-    price: product.price.sale,
-    qty: quantity,
-    totalPrice: product.price.sale * quantity,
-   };
-  }),
- });
-
- const customer: options["customer"] = {
-  name: req.user.name,
-  email: req.user.email,
-  metadata: {
-   userId: req.user.id,
-   orderId: newOrder._id.toString(),
-   type: "checkout",
-  },
- };
-
- const line_items: options["lineItems"] = cart.items.map(
-  ({ product, quantity }) => {
-   const images = product.cover
-    ? { images: [sanitizeUrl(product.cover.url)] }
-    : {};
-   return {
-    quantity,
-    price_data: {
-     currency: "usd",
-     unit_amount: product.price.sale,
-     product_data: {
-      name: product.title,
-      ...images,
-     },
-    },
-   };
+  if (!isValidObjectId(cartId)) {
+    return sendErrorResponse({
+      message: "Invalid cart id!!",
+      res,
+      status: 401,
+    });
   }
- );
 
- const session = await generateStripeCheckoutSession({
-  customer,
-  lineItems: line_items,
- });
+  const cart = await CartModel.findOne({
+    _id: cartId,
+    userId: req.user.id,
+  }).populate<{
+    items: { product: BookDoc; quantity: number }[];
+  }>({ path: "items.product" });
 
- if (session.url) {
-  res.json({ checkoutUrl: session.url });
- } else {
-  sendErrorResponse({
-   status: 500,
-   message: "Something went wrong could not handle payment",
-   res,
+  if (!cart) {
+    return sendErrorResponse({
+      message: "cart not found",
+      res,
+      status: 404,
+    });
+  }
+
+  const newOrder = await OrderModel.create({
+    userId: req.user.id,
+    orderItems: cart.items.map(({ product, quantity }) => {
+      return {
+        id: product._id,
+        price: product.price.sale,
+        qty: quantity,
+        totalPrice: product.price.sale * quantity,
+      };
+    }),
   });
- }
 
- //
+  const customer: options["customer"] = {
+    name: req.user.name,
+    email: req.user.email,
+    metadata: {
+      userId: req.user.id,
+      orderId: newOrder._id.toString(),
+      type: "checkout",
+    },
+  };
+
+  const line_items: options["lineItems"] = cart.items.map(
+    ({ product, quantity }) => {
+      const images = product.cover
+        ? { images: [sanitizeUrl(product.cover.url)] }
+        : {};
+      return {
+        quantity,
+        price_data: {
+          currency: "usd",
+          unit_amount: product.price.sale,
+          product_data: {
+            name: product.title,
+            ...images,
+          },
+        },
+      };
+    }
+  );
+
+  const session = await generateStripeCheckoutSession({
+    customer,
+    lineItems: line_items,
+  });
+
+  if (session.url) {
+    res.json({ checkoutUrl: session.url });
+  } else {
+    sendErrorResponse({
+      status: 500,
+      message: "Something went wrong could not handle payment",
+      res,
+    });
+  }
+
+  //
 };
 
 export const instantCheckout: RequestHandler = async (req, res) => {
- const { productId } = req.body;
+  const { productId } = req.body;
 
- if (!isValidObjectId(productId)) {
-  return sendErrorResponse({
-   message: "Invalid product id!!",
-   res,
-   status: 401,
-  });
- }
+  if (!isValidObjectId(productId)) {
+    return sendErrorResponse({
+      message: "Invalid product id!!",
+      res,
+      status: 401,
+    });
+  }
 
- const product = await BookModel.findById(productId);
- if (!product) {
-  return sendErrorResponse({
-   res,
-   message: "Product not found!",
-   status: 404,
-  });
- }
+  const product = await BookModel.findById(productId);
+  if (!product) {
+    return sendErrorResponse({
+      res,
+      message: "Product not found!",
+      status: 404,
+    });
+  }
 
- product.copiesSold += 1;
- await product.save();
+  product.copiesSold += 1;
+  product.price = product.price;
+  await product.save();
 
- const newOrder = await OrderModel.create({
-  userId: req.user.id,
-  orderItems: {
-   id: product._id,
-   price: product.price.sale,
-   qty: 1,
-   totalPrice: product.price.sale * 1,
-  },
- });
-
- const images = product.cover
-  ? { images: [sanitizeUrl(product.cover.url)] }
-  : {};
-
- const line_items: StripeLineItems = [
-  {
-   quantity: 1,
-   price_data: {
-    currency: "usd",
-    unit_amount: product.price.sale,
-    product_data: {
-     name: product.title,
-     ...images,
+  const newOrder = await OrderModel.create({
+    userId: req.user.id,
+    orderItems: {
+      id: product._id,
+      price: product.price.sale,
+      qty: 1,
+      totalPrice: product.price.sale * 1,
     },
-   },
-  },
- ];
-
- const customer: options["customer"] = {
-  name: req.user.name,
-  email: req.user.email,
-  metadata: {
-   userId: req.user.id,
-   orderId: newOrder._id.toString(),
-   type: "instant-checkout",
-  },
- };
-
- const session = await generateStripeCheckoutSession({
-  customer,
-  lineItems: line_items,
- });
- if (session.url) {
-  res.json({ checkoutUrl: session.url });
- } else {
-  sendErrorResponse({
-   res,
-   message: "Something went wrong, could not handle payment!",
-   status: 500,
   });
- }
+
+  const images = product.cover
+    ? { images: [sanitizeUrl(product.cover.url)] }
+    : {};
+
+  const line_items: StripeLineItems = [
+    {
+      quantity: 1,
+      price_data: {
+        currency: "usd",
+        unit_amount: product.price.sale,
+        product_data: {
+          name: product.title,
+          ...images,
+        },
+      },
+    },
+  ];
+
+  const customer: options["customer"] = {
+    name: req.user.name,
+    email: req.user.email,
+    metadata: {
+      userId: req.user.id,
+      orderId: newOrder._id.toString(),
+      type: "instant-checkout",
+    },
+  };
+
+  const session = await generateStripeCheckoutSession({
+    customer,
+    lineItems: line_items,
+  });
+  if (session.url) {
+    res.json({ checkoutUrl: session.url });
+  } else {
+    sendErrorResponse({
+      res,
+      message: "Something went wrong, could not handle payment!",
+      status: 500,
+    });
+  }
 };
