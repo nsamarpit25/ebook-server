356ba6591b5a4dd3ebe0f872d71f89e0070a78ce
Samarpit <nsamarpit25@gmail.com>
2025-01-27 17:39:25 +0530

    fix: enhance book model schema for improved data structure and validation

--- src/models/book.model.ts
+++ src/models/book.model.ts
@@ -107,118 +107,120 @@
 import { Model, ObjectId, Schema, Types, model } from "mongoose";
 
 export interface BookDoc {
- _id?: Types.ObjectId;
- author: Types.ObjectId;
- title: string;
- slug: string;
- description: string;
- language: string;
- publishedAt: Date;
- publicationName: string;
- averageRating?: number;
- genre: string;
- status: "published" | "unpublished";
- copiesSold: number;
- price: {
-  mrp: number;
-  sale: number;
- };
- cover?: {
-  id: string;
-  url: string;
- };
- fileInfo: {
-  id: string;
-  size: string;
- };
+  _id?: Types.ObjectId;
+  author: Types.ObjectId;
+  title: string;
+  slug: string;
+  description: string;
+  language: string;
+  publishedAt: Date;
+  publicationName: string;
+  averageRating?: number;
+  genre: string;
+  status: "published" | "unpublished";
+  copiesSold: number;
+  price: {
+    mrp: number;
+    sale: number;
+  };
+  cover?: {
+    id: string;
+    url: string;
+  };
+  fileInfo: {
+    id: string;
+    size: string;
+  };
 }
 
 const bookSchema = new Schema<BookDoc>({
- author: {
-  type: Schema.Types.ObjectId,
-  ref: "Author",
-  required: true,
- },
- title: {
-  type: String,
-  required: true,
-  trim: true,
- },
- slug: {
-  type: String,
-  required: true,
-  trim: true,
-  unique: true,
- },
- description: {
-  type: String,
-  required: true,
-  trim: true,
- },
- language: {
-  type: String,
-  required: true,
-  trim: true,
- },
- publicationName: {
-  type: String,
-  required: true,
-  trim: true,
- },
- averageRating: Number,
- genre: {
-  type: String,
-  required: true,
-  trim: true,
- },
- copiesSold: {
-  type: Number,
-  default: 0,
- },
- status: {
-  type: String,
-  default: "published",
-  enum: ["published", "unpublished"],
- },
- publishedAt: {
-  type: Date,
-  required: true,
- },
- price: {
-  type: Object,
-  required: true,
-  mrp: {
-   type: Number,
-   required: true,
+  author: {
+    type: Schema.Types.ObjectId,
+    ref: "Author",
+    required: true,
   },
-  sale: {
-   type: Number,
-   required: true,
+  title: {
+    type: String,
+    required: true,
+    trim: true,
   },
- },
- cover: {
-  url: String,
-  id: String,
- },
- fileInfo: {
-  type: Object,
-  required: true,
-  url: {
-   type: String,
-   required: true,
+  slug: {
+    type: String,
+    required: true,
+    trim: true,
+    unique: true,
   },
-  id: {
-   type: String,
-   required: true,
+  description: {
+    type: String,
+    required: true,
+    trim: true,
   },
- },
+  language: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  publicationName: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  averageRating: Number,
+  genre: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  copiesSold: {
+    type: Number,
+    default: 0,
+  },
+  status: {
+    type: String,
+    default: "published",
+    enum: ["published", "unpublished"],
+  },
+  publishedAt: {
+    type: Date,
+    required: true,
+  },
+  price: {
+    type: Object,
+    required: true,
+    mrp: {
+      type: Number,
+      required: true,
+    },
+    sale: {
+      type: Number,
+      required: true,
+    },
+  },
+  cover: {
+    url: String,
+    id: String,
+  },
+  fileInfo: {
+    type: Object,
+    required: true,
+    url: {
+      type: String,
+      required: true,
+    },
+    id: {
+      type: String,
+      required: true,
+    },
+  },
 });
 
 bookSchema.pre("save", function (next) {
- const { mrp, sale } = this.price;
- this.price = { mrp: mrp * 100, sale: sale * 100 };
+  if (this.isNew) {
+    const { mrp, sale } = this.price;
+    this.price = { mrp: mrp * 100, sale: sale * 100 };
 
- next();
+    next();
+  }
 });
 
 const BookModel = model("Book", bookSchema);
